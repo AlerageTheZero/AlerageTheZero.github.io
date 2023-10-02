5e86620af82e3e4181b922faf7cbbadc2c9f1737
AlerageTheZero <58176605+AlerageTheZero@users.noreply.github.com>
2023-10-02 14:18:31 +0200

    Add files via upload

---
+++ swipeInit.js
@@ -0,0 +1,34 @@
+var swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  mousewheel: true,
+  keyboard: true
+});
+
+
+/* === FUNZIONA, MA DEVO STUDIARE MEGLIO I MUTATION OBSERVER === */
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutationRecord) {
+      //console.log('style changed!');
+      swiper.update();
+   });
+ });
+
+ var target = document.getElementById('myDiary');
+ observer.observe(target, { attributes : true, attributeFilter : ['style'] });
+
+
+/* IDEA INIZIALE MA CHANGE NON FUNZIONA SUGLI ATTRIBUTI CSS, CREDO*/
+/*
+document.getElementById("myDiary").addEventListener("change", function(event){
+  if (event.target.style.display == "block")
+    swiper.update();
+});*/
